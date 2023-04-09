b879ddbbc29652ee80082709d0e0155d93d8197c
ratan deep singh <125176079+ratandeep1107@users.noreply.github.com>
2023-04-09 16:44:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+function alert() {
+	//your JS code here. If required.
+let firstname = document.getElementById("first-name").value;
+let lastname = document.getElementById("last-name").value;
+let phonenumber = document.getElementById("phone-number").value;
+let emailid = document.getElementById("email-id").value;
+
+	let record = "First Name: " + firstname + "\nLast Name: " + lastname + "\nPhone Number: "+ phonenumber + "\nEmail ID: "+ emailid;
+	alert (record);
+}
